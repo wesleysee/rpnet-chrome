858032da691f6fedd5c307de1ad0edb1f4d8fd99
Wesley Spencer See <wesleysee@gmail.com>
2012-07-27 18:21:46 +0800

    integrating twitter bootstrap

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
 var rpnetLinks = [];
+var currentIndex = 0;
 
 function getLinks(info, tab) {
   var selectedText = info.selectionText;
   var linksArr = selectedText.split(" ");
   var links = "";
   var len = linksArr.length
+  rpnetLinks[currentIndex] = [];
   for (var i = 0; i < len; i++) {
   	var link = linksArr[i];
   	if (isValidLink(link)) {
-	  	//generateLinks(link);
-      handleGeneratedLink(link);
+	  	//generateLinks(link, currentIndex);
+      handleGeneratedLink(link, currentIndex);
   	}
   }
+  currentIndex += 1;
 }
 
 function isValidLink(link) {
@@ -21,7 +24,7 @@ function isValidLink(link) {
 var title = "Download with RPNet";
 var id = chrome.contextMenus.create({"title": title, "contexts":["selection"], "onclick": getLinks});
 
-function generateLinks(links) {
+function generateLinks(links, index) {
 	var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("POST", "https://premium.rpnet.biz/client_api.php?action=generate");
     xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
@@ -33,7 +36,7 @@ function generateLinks(links) {
           if (response.hasOwnProperty('error')) {
             throw Error(response.error);
           } else if (response.links[0].hasOwnProperty('generated')) {
-            handleGeneratedLink(response.links[0].generated);            
+            handleGeneratedLink(response.links[0].generated, index);            
           } else {
             throw Error(response.links[0].error);
           }
@@ -50,11 +53,15 @@ function generateLinks(links) {
     xmlhttp.send(dataString);
 }
 
-function handleGeneratedLink(link) {
-  rpnetLinks.push(link);
+function handleGeneratedLink(link, index) {
+  rpnetLinks[index].push(link);
   chrome.extension.sendMessage({links: rpnetLinks});
 }
 
 function getRPNetLinks() {
   return rpnetLinks;
+}
+
+function getCurrentIndex() {
+  return currentIndex;
 }
--- popup.js
+++ popup.js
@@ -12,10 +12,23 @@ function populate(links) {
 		return;
 	}
 	var len = links.length;
-	var html = '';
+	var html = "<div class=\"tabbable\"><ul class=\"nav nav-tabs\"><li class=\"active\"><a href=\"#tab1\" data-toggle=\"tab\">Section 1</a></li>"
+  html += "<li><a href=\"#tab2\" data-toggle=\"tab\">Section 2</a></li></ul><div class=\"tab-content\">";
+	var currentIndex = chrome.extension.getBackgroundPage().getCurrentIndex();
 	for (var i = 0; i < len; i++) {
-		var link = links[i];
-		html += "<a href=\"" + link + "\">" + link + "</a>\n";
+		var linkArr = links[i];
+		var len_j = linkArr.length;
+		if (i == currentIndex - 1) {
+			html += "<div class=\"tab-pane active\" id=\"tab" + (i + 1) + "\">";
+		} else {
+			html += "<div class=\"tab-pane\" id=\"tab" + (i + 1) + "\">";
+		}
+		for (var j = 0; j < len_j; j++) {
+			var link = linkArr[j];
+			html += "<a href=\"" + link + "\">" + link + "</a>\n";			
+		}
+		html += "</div>";
 	}
+	html += "</div>";
 	document.write(html);
 }
